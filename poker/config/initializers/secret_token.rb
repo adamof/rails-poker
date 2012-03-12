ac4ff7d66df713a3da6a31eafb28ba82bbd0064a
badjo <stefan.adamof@gmail.com>
2012-03-11 18:01:59 -0700

    created the rails app

---
+++ poker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poker::Application.config.secret_token = '59d44ccd07cf29d43b2c91d917b58ae5d8f8cdbe00049be34c1dfc35afc8d1fc428c9c20acd3bfbc94f544d12d7c0dafbbdc2b0f143c0de451737a1adccc6948'
