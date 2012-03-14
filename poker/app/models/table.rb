3cc34d72c09132b666a780910363768f5989e8ef
badjo <stefan.adamof@gmail.com>
2012-03-13 17:33:27 -0700

    minor changs

--- poker/app/models/table.rb
+++ poker/app/models/table.rb
@@ -2,4 +2,15 @@ class Table < ActiveRecord::Base
   belongs_to  :tournament
   has_many    :players
   has_many    :pots  
+  after_update :broadcast
+
+  def broadcast
+    # Juggernaut.publish("channel1", "updated table #{self.id}")
+    # Juggernaut.publish("channel1", self.to_json)
+    Juggernaut.publish("channel1", "updated table #{self.id} on 1")
+    Juggernaut.publish("channel1", "changes are #{self.changes}")
+
+    # Juggernaut.publish("channel2", "updated table #{self.id} on 2")
+
+  end
 end
