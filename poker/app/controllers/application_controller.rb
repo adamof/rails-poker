faabdf81e3f4ca7d23e743d6cdb3e08a4bd818a3
root <root@li336-180.members.linode.com>
2012-03-17 03:36:13 -0400

    fix the redirect

--- poker/app/controllers/application_controller.rb
+++ poker/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   
   def after_sign_in_path_for(resource)
-    return current_player.table || root_path
+    return current_player.table || Table.first || root_path
   end
   
   def after_sign_out_path_for(resource)
---
+++ poker/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+nohup rails s &
+nohup ../../redis-2.4.8/src/redis-server &
+nohup juggernaut &
